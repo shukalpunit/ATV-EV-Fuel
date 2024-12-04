a85cd4bdb191022a9f47def6377e5098db21c3f1
Punit Shukal <punit.shukal15475@sakec.ac.in>
2024-12-03 22:19:33 -0700

    App js updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Discover from "./pages/Discover";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Discover from './pages/Discover';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <div className="app">
+      <div className="App">
         <Header />
-        <main>
+        <div className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/discover" element={<Discover />} />
           </Routes>
-        </main>
+        </div>
         <Footer />
       </div>
     </Router>
   );
-};
+}
 
 export default App;
